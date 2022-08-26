35c7541b780055cfa42de541b4f20291cd5c5ac0
Milton Virues <viruesmilton63@gmail.com>
2022-08-26 08:49:48 -0300

    hice un git pull de main y ahora voy pushear en pulsador

--- main.ino
+++ main.ino
@@ -1,4 +1,3 @@
-
 class Motor
 {
 private:
@@ -73,6 +72,59 @@ public:
         digitalWrite(pin_dir, LOW);
         analogWrite(pin_pwm, 0);
     }
+};
+
+
+class Led {
+  private:
+    int pin;
+
+  public:
+    Led(int p) {
+      pin = p;
+      pinMode(pin, OUTPUT);
+    }
+    void setPrenderLed() {
+      digitalWrite(pin, HIGH);
+    }
+    void setApagarLed() {
+      digitalWrite(pin, LOW);
+    }
+
+};
+
+class Pulsador {
+  private:
+    int pin;
+    bool flanco = HIGH;
+    bool estado_anterior = !flanco;
+
+    //metodo
+  public:
+    Pulsador(int p) {
+      pin = p;
+
+      pinMode(pin, INPUT);
+    }
+
+    void setFlanco(bool f) {
+      flanco = f;
+      estado_anterior = !flanco;
+    }
+
+    //metodos o acciones
+    bool getIsPress() {
+      bool estado_actual = digitalRead(pin);
+      bool estado = (estado_anterior != estado_actual) && estado_actual == flanco;
+      estado_anterior = estado_actual;
+      return estado;
+    }
+
+    String getIsPressText() {
+      if (getIsPress()) return "PRESS";
+      else return "IS NOT PRESS";
+    }
+
 };
 
 void setup(){
